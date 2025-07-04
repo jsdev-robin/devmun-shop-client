c5063594dfddaf64811b062ab167db74967b9533
Md Robin <jsdev.robin@gmail.com>
2025-07-04 09:59:13 +0600

    feat: add OrderReport component for detailed order analytics

--- apps/seller/src/app/(main)/page.tsx
+++ apps/seller/src/app/(main)/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SalesReport from '../../components/pages/overview/SalesReport';
+import OrderReport from '../../components/pages/overview/OrderReport';
 
 const OverviewPage = () => {
   return (
     <>
       <SalesReport />
+      <OrderReport />
     </>
   );
 };
