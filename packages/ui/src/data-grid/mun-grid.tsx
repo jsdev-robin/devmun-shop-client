96151bc69c8ef909e3755c0c329ba847823f55eb
Md Robin <jsdev.robin@gmail.com>
2025-07-06 01:38:08 +0600

    feat: Add break point for MunGridRowNumber

--- packages/ui/src/data-grid/mun-grid.tsx
+++ packages/ui/src/data-grid/mun-grid.tsx
@@ -5,6 +5,8 @@ import { DataGridProvider } from './contexts/data-grid-contexts';
 import ColumnDnd from './contexts/data-grid-column-dnd';
 import MunGridMain from './mun-grid/mun-grid-main';
 import MunGridRowNumber from './mun-grid/mun-grid-row-number';
+import { useBreakpoint } from '../hooks/use-breakpoint';
+import { breakpoints } from '../utils/breakpoints';
 
 interface MunGridProps<T> {
   data?: T[];
@@ -20,6 +22,7 @@ const MunGrid = <T,>({
   isError,
   isLoading,
 }: MunGridProps<T>) => {
+  const sm = !useBreakpoint(breakpoints.sm);
   return (
     <DataGridProvider
       data={data}
@@ -30,7 +33,7 @@ const MunGrid = <T,>({
       <ColumnDnd>
         <div className="space-y-3 relative">
           <div className="flex bg-muted rounded-md overflow-hidden border border-border">
-            <MunGridRowNumber />
+            {sm && <MunGridRowNumber />}
             <div className="overflow-hidden flex-1">
               <MunGridMain />
             </div>
