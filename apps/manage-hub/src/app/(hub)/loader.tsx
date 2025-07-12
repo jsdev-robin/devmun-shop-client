bf463b6ab3598c9fdcbffa717507a8975ff426dd
Md Robin <jsdev.robin@gmail.com>
2025-07-12 21:37:35 +0600

    fix unknow error when page loading

--- apps/manage-hub/src/app/(hub)/loader.tsx
+++ apps/manage-hub/src/app/(hub)/loader.tsx
@@ -1,22 +1,26 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { userAuthApi } from '../../lib/features/services/auth/authApi';
 import Loading from '../loading';
 import ApiError from '@repo/ui/components/api-error';
 
 const Loader = ({ children }: { children: React.ReactNode }) => {
-  const { isLoading, isError } = userAuthApi.endpoints.getProfile.useQuery();
+  const profile = userAuthApi.endpoints.getProfile.useQuery();
+  const [triggerRefresh] = userAuthApi.endpoints.refreshToken.useMutation();
 
-  if (isError) {
-    return (
-      <>
-        <ApiError />
-      </>
-    );
+  useEffect(() => {
+    const refreshPromise = triggerRefresh();
+    return () => {
+      refreshPromise.abort();
+    };
+  }, [triggerRefresh]);
+
+  if (profile.isError) {
+    return <ApiError />;
   }
 
-  return isLoading ? <Loading /> : children;
+  return profile.isLoading ? <Loading /> : children;
 };
 
 export default Loader;
