7d4cadc9deed45a5dafab81de2fd6a02eebca34a
Md Robin <jsdev.robin@gmail.com>
2025-07-12 23:09:54 +0600

    fix client error for user loading

--- apps/manage-hub/src/app/(hub)/loader.tsx
+++ apps/manage-hub/src/app/(hub)/loader.tsx
@@ -1,20 +1,20 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import { userAuthApi } from '../../lib/features/services/auth/authApi';
 import Loading from '../loading';
 import ApiError from '@repo/ui/components/api-error';
 
 const Loader = ({ children }: { children: React.ReactNode }) => {
   const profile = userAuthApi.endpoints.getProfile.useQuery();
-  const [triggerRefresh] = userAuthApi.endpoints.refreshToken.useMutation();
+  // const [triggerRefresh] = userAuthApi.endpoints.refreshToken.useMutation();
 
-  useEffect(() => {
-    const refreshPromise = triggerRefresh();
-    return () => {
-      refreshPromise.abort();
-    };
-  }, [triggerRefresh]);
+  // useEffect(() => {
+  //   const refreshPromise = triggerRefresh();
+  //   return () => {
+  //     refreshPromise.abort();
+  //   };
+  // }, [triggerRefresh]);
 
   if (profile.isError) {
     return <ApiError />;
